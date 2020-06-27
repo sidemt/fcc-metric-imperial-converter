facbb792baa460f232d1e5bab9adb5dec31af4df
Glitch (nifty-clear-cruiser) <none>
2020-06-27 19:57:38 +0000

    💏🚟 Checkpoint ./tests/2_functional-tests.js:763627/274

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -70,17 +70,17 @@ suite('Functional Tests', function() {
       });
       
       test('Convert kg (no number)', function(done) {
-         chai.request(server)
-        .get('/api/convert')
-        .query({input: '10L'})
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
-          assert.approximately(res.body.returnNum, 2.64172, 0.1);
-          assert.equal(res.body.returnUnit, 'gal');
-          done();
-        });
+        chai.request(server)
+          .get('/api/convert')
+          .query({input: 'kg'})
+          .end(function(err, res){
+            assert.equal(res.status, 200);
+            assert.equal(res.body.initNum, 1);
+            assert.equal(res.body.initUnit, 'kg');
+            assert.approximately(res.body.returnNum, 2.20462, 0.1);
+            assert.equal(res.body.returnUnit, 'lbs');
+            done();
+          });
         //done();
       });
       
